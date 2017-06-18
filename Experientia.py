a3ce62252f2955c374b6e486eda9f50a9479738d
Zack Rauen <rauenzi@outlook.com>
2017-06-18 18:56:09 -0400

    Moved defaults to settings.json

--- Experientia.py
+++ Experientia.py
@@ -13,10 +13,6 @@ from discord.ext import commands
 from discordbot.discordbot import DiscordBot
 from discordbot.bot_utils import checks
 
-description = """
-Experience system based around sending messages and gaining roles.
-"""
-
 try:
     import uvloop
 except ImportError:
@@ -24,13 +20,8 @@ except ImportError:
 else:
     asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
-cogs = [
-    'cogs.levels'
-]
-
-prefix = '~'
-bot = DiscordBot(command_prefix=prefix, description=description, pm_help=False, help_attrs=dict(hidden=True))
+bot = DiscordBot(command_prefix=None, description=None, pm_help=False, help_attrs=dict(hidden=True))
 
 if __name__ == '__main__':
-    bot.load_cogs(cogs)
+    bot.load_cogs()
     bot.run()
